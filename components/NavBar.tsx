ba5d583f3ce89418458518552a1d446453d0db12
code-overflow247 <104567538+code-overflow247@users.noreply.github.com>
2023-03-30 11:11:54 +0000

    added desktop navbar

---
+++ components/NavBar.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import styles from "@/styles/Home.module.css";
+import Image from "next/image";
+import { Menu, MenuButton, MenuList, MenuItem, Button } from "@chakra-ui/react";
+import { ChevronDown } from "react-iconly";
+
+function NavBar() {
+  return (
+    <div className={styles.navbar}>
+      <div>
+        <Image src="/logo1.svg" alt="" width={40} height={40} />
+      </div>
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+        }}
+      >
+        <Menu>
+          <MenuButton  bg={"white"}  as={Button} rightIcon={<ChevronDown />}>
+            Accra
+          </MenuButton>
+          <MenuList>
+            <MenuItem>Delhi</MenuItem>
+          </MenuList>
+        </Menu>
+        <button style={{marginLeft:"10px"}} className={styles.button}>Login</button>
+      </div>
+    </div>
+  );
+}
+
+export default NavBar;
