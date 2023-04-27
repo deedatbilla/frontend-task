99929e001d1d986d13ddff6ec3baea8e4707fee5
code-overflow247 <deedat5@yahoo.com>
2023-04-27 09:26:41 +0000

    misc

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,36 +2,65 @@ import React from "react";
 
 function Footer() {
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        gap: "6px",
-        background: "#F4F4F5]",
-        paddingTop: "20px",
-        paddingBottom: "20px",
-        paddingInline:"10px"
-      }}
-    >
-      <div>
-        <p style={{ fontWeight: "bold", fontSize: "18px" }}>Company</p>
-        <p style={{ fontSize: "16px" }}>Community</p>
-        <p style={{ fontSize: "16px" }}>Blog</p>
-        <p style={{ fontSize: "16px" }}>Events</p>
+    <footer style={{ paddingTop: "30px", paddingBottom: "20px" }}>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          gap: "6px",
+          background: "#151515",
+
+          paddingInline: "10px",
+          color: "white",
+        }}
+      >
+        <div>
+          <p style={{ fontWeight: "bold", fontSize: "18px" }}>Company</p>
+          <p style={{ fontSize: "16px" }}>Community</p>
+          <p style={{ fontSize: "16px" }}>Blog</p>
+          <p style={{ fontSize: "16px" }}>Events</p>
+        </div>
+        <div>
+          <p style={{ fontWeight: "bold", fontSize: "18px" }}>Support</p>
+          <p style={{ fontSize: "16px" }}>Help center</p>
+          <p style={{ fontSize: "16px" }}>Report</p>
+          <p style={{ fontSize: "16px" }}>Status</p>
+        </div>
+        <div>
+          <p style={{ fontWeight: "bold", fontSize: "18px" }}>Follow</p>
+          <p
+            style={{
+              fontSize: "16px",
+              color: "#FE5923",
+              textDecoration: "underline",
+            }}
+          >
+            Facebook
+          </p>
+          <p
+            style={{
+              fontSize: "16px",
+              color: "#FE5923",
+              textDecoration: "underline",
+            }}
+          >
+            Twitter
+          </p>
+          <p
+            style={{
+              fontSize: "16px",
+              color: "#FE5923",
+              textDecoration: "underline",
+            }}
+          >
+            Instagram
+          </p>
+        </div>
       </div>
-      <div>
-        <p style={{ fontWeight: "bold", fontSize: "18px" }}>Support</p>
-        <p style={{ fontSize: "16px" }}>Help center</p>
-        <p style={{ fontSize: "16px" }}>Report</p>
-        <p style={{ fontSize: "16px" }}>Status</p>
+      <div style={{ paddingInline: "10px",marginTop:"12px" }}>
+        <img src="/clogo.png" alt="" style={{ width: "150px" }} />
       </div>
-      <div>
-        <p style={{ fontWeight: "bold", fontSize: "18px" }}>Follow</p>
-        <p style={{ fontSize: "16px",color:"#FE5923",textDecoration:"underline" }}>Facebook</p>
-        <p style={{ fontSize: "16px" ,color:"#FE5923",textDecoration:"underline"}}>Twitter</p>
-        <p style={{ fontSize: "16px",color:"#FE5923",textDecoration:"underline" }}>Instagram</p>
-      </div>
-    </div>
+    </footer>
   );
 }
 
